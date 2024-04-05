7c1b246593cc6553a578126a26a1ccfba8de18fc
charlyol <147928338+charlyol@users.noreply.github.com>
2024-04-05 16:23:08 +0200

    yoloweell

--- fetchTest/catalogue/data.js
+++ fetchTest/catalogue/data.js
@@ -1,144 +1,130 @@
-async function callApi(toto) {
+// Fonction pour cacher les divs initialement
+function hideInitialElements() {
+    const div = document.getElementById("div");
+    div.style.display = 'none';
+    const header = document.getElementById("header");
+    header.style.display = 'none';
+    const slider = document.getElementById("slider__wrapper");
+    slider.style.display = 'none';
+}
+// Fonction pour créer et afficher le loader
+function displayLoader() {
+    const loader = document.createElement('img');
+    loader.src = "FnnI.gif";
+    loader.id = "loader";
+    document.body.appendChild(loader);
+    return loader;
+}
+// Fonction pour cacher le loader une fois que le contenu est chargé
+function hideLoader(loader) {
+    loader.style.display = 'none';
+}
+// Fonction pour afficher les données des pokémons depuis l'API
+async function displayPokemonFromAPI(apiUrl, loader) {
     try {
-        // Cacher les divs initialement
-        const div = document.getElementById("div");
-        div.style.display = 'none';
-        const header = document.getElementById("header");
-        header.style.display = 'none';
-        const slider = document.getElementById("slider__wrapper");
-        slider.style.display = 'none';
-
-        // Créer et afficher le loader
-        const loader = document.createElement('img');
-        loader.src = "FnnI.gif";
-        loader.id = "loader";
-        document.body.appendChild(loader);
-
-        const api = await fetch(toto);
-        if (!api.ok) {
+        const apiData = await fetch(apiUrl);
+        if (!apiData.ok) {
             throw new Error('Erreur lors de la requête API');
         }
-        const data = await api.json();
+        const data = await apiData.json();
 
-        console.log(data);
-        console.log(localStorage)
-        console.log(localStorage.pokemon)
+        const div = document.getElementById("div");
+        div.style.display = 'flex';
+        const header = document.getElementById("header");
+        header.style.display = 'block';
+        const slider = document.getElementById("slider__wrapper");
+        slider.style.display = 'block';
 
-        setTimeout(() => {
-            div.style.display = 'grid';
-            header.style.display = 'block';
-            slider.style.display = 'block';
-            loader.style.display = 'none';
+        hideLoader(loader);
 
-            let j = 0
-            while (j < localStorage.length) {
-                const pokemonData = JSON.parse(localStorage.getItem('pokemon'));
-                if (pokemonData) {
-                    pokemonData.forEach((pokemon, index) => {
-                        const newDiv = document.createElement("div");
-                        newDiv.classList.add("local");
-                        div.appendChild(newDiv);
-
-                        const supElement = document.createElement("button");
-                        supElement.textContent = "❌";
-                        supElement.addEventListener("click", function() {
-                            newDiv.remove();
-                            pokemonData.splice(index, 1);
-                            localStorage.setItem('pokemon', JSON.stringify(pokemonData));
-                        });
-                        newDiv.appendChild(supElement);
-
-                        const nameElement = document.createElement("p");
-                        nameElement.textContent = `Name: ${pokemon.name}`;
-                        newDiv.appendChild(nameElement);
-
-                        const pokedexIdElement = document.createElement("p");
-                        pokedexIdElement.textContent = `First Name: ${pokemon.pokedex_id}`;
-                        newDiv.appendChild(pokedexIdElement);
-
-                        const imageElement = document.createElement("img");
-                        imageElement.src = pokemon.image;
-                        newDiv.appendChild(imageElement);
-
-                    });
-                } else {
-                    console.log("Aucun Pokémon trouvé dans le localStorage.");
-                }
-                j++;
-            }
-
-
-            let i = 1
-            while (i < 50) {
-                const newDiv = document.createElement("div");
-                newDiv.classList.add("card");
-                div.appendChild(newDiv);
-
-                let r = Math.floor(Math.random() * 1025) + 1;
-
-                const pokedexIdElement = document.createElement("p");
-                pokedexIdElement.textContent = `Pokedex ID: ${data[r].pokedex_id}`;
-                newDiv.appendChild(pokedexIdElement);
-
-                const nameElement = document.createElement("p");
-                nameElement.textContent = `Name: ${data[r].name.fr}`;
-                newDiv.appendChild(nameElement);
-
-                const pokemonImage = document.createElement("img");
-                pokemonImage.src = `${data[r].sprites.regular}`;
-                pokemonImage.classList.add("pokemon-image");
-                newDiv.appendChild(pokemonImage);
-
-                const imageTypes = [];
-                if (data[r].sprites.regular !== null) {
-                    imageTypes.push({
-                        "regular": `${data[r].sprites.regular}`
-                    });
-                }
-                if (data[r].sprites.shiny !== null) {
-                    imageTypes.push({
-                        "shiny": `${data[r].sprites.shiny}`
-                    });
-                }
-                if (data[r].sprites.gmax !== null) {
-                    imageTypes.push({
-                        "gmax.regular": `${data[r].sprites.gmax.regular}`
-                    });
-                    imageTypes.push({
-                        "gmax.shiny": `${data[r].sprites.gmax.shiny}`
-                    });
-                    if (data[r].sprites.gmax.regular && data[r].sprites.gmax.shiny == null) {
-                        imageTypes.push({
-                            "gmax": `${data[r].sprites.gmax}`
-                        });
-                    }
-                }
-                console.log(imageTypes);
-
-                let imageIndex = 0;
-                pokemonImage.addEventListener('click', function () {
-                    imageIndex = (imageIndex + 1) % imageTypes.length;
-                    pokemonImage.src = Object.values(imageTypes[imageIndex])[0];
-                });
-
-                i++
-            }
-        }, 2500);
-
+        for (let i = 0; i < 150; i++) {
+            const randomPokemon = data[Math.floor(Math.random() * data.length)];
+            displayPokemon(randomPokemon, div);
+        }
     } catch (error) {
         console.error('Erreur lors de la récupération des données:', error);
     }
 }
+// Fonction pour afficher les pokémons depuis le local storage
+function displayPokemonFromStorage() {
+    const div = document.getElementById("div");
+    const pokemonData = getPokemonFromStorage();
 
-callApi("https://tyradex.tech/api/v1/pokemon");
+    pokemonData.forEach((pokemon, index) => {
+        const newDiv = document.createElement("div");
+        newDiv.classList.add("local");
+        newDiv.dataset.aos = "flip-left";
+        newDiv.dataset.aos.offset="300";
+        div.appendChild(newDiv);
 
-function getPokemonFromStorage() {
-    let pokemonString = localStorage.getItem('pokemon');
-    return pokemonString ? JSON.parse(pokemonString) : [];
+        // Créer les éléments pour chaque pokemon...
+        const supElement = document.createElement("button");
+        supElement.textContent = "❌";
+        supElement.addEventListener("click", function() {
+            newDiv.remove();
+            pokemonData.splice(index, 1);
+            updateLocalStorage(pokemonData);
+        });
+
+        newDiv.appendChild(supElement);
+
+        const nameElement = document.createElement("p");
+        nameElement.textContent = `Name: ${pokemon.name}`;
+        newDiv.appendChild(nameElement);
+
+        const pokedexIdElement = document.createElement("p");
+        pokedexIdElement.textContent = `First Name: ${pokemon.pokedex_id}`;
+        newDiv.appendChild(pokedexIdElement);
+
+        const imageElement = document.createElement("img");
+        imageElement.src = pokemon.image;
+        newDiv.appendChild(imageElement);
+    });
 }
+// Fonction pour afficher les données d'un Pokémon
+function displayPokemon(pokemon, parentElement) {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("card");
+    newDiv.dataset.aos = "flip-left";
+    newDiv.dataset.aos.offset="300";
+    parentElement.appendChild(newDiv);
 
+    const pokedexIdElement = document.createElement("p");
+    pokedexIdElement.textContent = `Pokedex ID: ${pokemon.pokedex_id}`;
+    newDiv.appendChild(pokedexIdElement);
 
-function slider() {
+    const nameElement = document.createElement("p");
+    nameElement.textContent = `Name: ${pokemon.name}`;
+    newDiv.appendChild(nameElement);
+
+    const pokemonImage = document.createElement("img");
+    pokemonImage.src = pokemon.sprites.regular;
+    pokemonImage.classList.add("pokemon-image");
+    newDiv.appendChild(pokemonImage);
+
+    const imageTypes = [];
+    if (pokemon.sprites.regular !== null) {
+        imageTypes.push({ "regular": pokemon.sprites.regular });
+    }
+    if (pokemon.sprites.shiny !== null) {
+        imageTypes.push({ "shiny": pokemon.sprites.shiny });
+    }
+    if (pokemon.sprites.gmax !== null) {
+        imageTypes.push({ "gmax.regular": pokemon.sprites.gmax.regular });
+        imageTypes.push({ "gmax.shiny": pokemon.sprites.gmax.shiny });
+        if (pokemon.sprites.gmax.regular && pokemon.sprites.gmax.shiny == null) {
+            imageTypes.push({ "gmax": pokemon.sprites.gmax });
+        }
+    }
+
+    let imageIndex = 0;
+    pokemonImage.addEventListener('click', function () {
+        imageIndex = (imageIndex + 1) % imageTypes.length;
+        pokemonImage.src = Object.values(imageTypes[imageIndex])[0];
+    });
+}
+// Fonction pour initialiser le slider
+function initializeSlider() {
     let slides = document.querySelectorAll(".slide"),
         slider = document.querySelector(".slider"),
         last = slider.lastElementChild,
@@ -173,4 +159,26 @@ function slider() {
         }
     }
 }
-slider();
+// Fonction pour récupérer les données des pokémons depuis le localStorage
+function getPokemonFromStorage() {
+    let pokemonString = localStorage.getItem('pokemon');
+    return pokemonString ? JSON.parse(pokemonString) : [];
+}
+// Fonction principale
+async function main() {
+    hideInitialElements();
+    const loader = displayLoader();
+    await new Promise(resolve => setTimeout(resolve, 2500));
+    hideLoader(loader);
+    displayPokemonFromStorage();
+    await displayPokemonFromAPI("https://tyradex.tech/api/v1/pokemon", loader);
+    initializeSlider();
+}
+// Appeler la fonction principale
+main();
+
+const btn = document.getElementById("btnSwitch")
+    btn.addEventListener("click",() => {
+        const div = document.getElementById("div")
+        div.classList.toggle('switchOrientation')
+    })
